d4f562839d09895636fdfa6b17cf255ac7cdbb18
phatyou <jen.phatmandu7@gmail.com>
2017-09-30 14:27:40 -0600

    additional files to commit

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers (burger_name) VALUES ('Holly\'s Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bridget\'s Hawai\'i Burger');
+INSERT INTO burgers (burger_name) VALUES ('Crystal\'s Burger');
+INSERT INTO burgers (burger_name) VALUES ('Spicy Kendra Burger');
+INSERT INTO burgers (burger_name) VALUES ('Playmate Chicken Wrap');
+INSERT INTO burgers (burger_name) VALUES ('The Bunny Burger');
+INSERT INTO burgers (burger_name) VALUES ('Playmate Chicken Wrap');
+INSERT INTO burgers (burger_name) VALUES ('Entourage Burger #1');
+INSERT INTO burgers (burger_name) VALUES ('Entourage Burger #2');
+INSERT INTO burgers (burger_name) VALUES ('Entourage Burger #3');
+INSERT INTO burgers (burger_name) VALUES ('"Club Night" Club');
+
